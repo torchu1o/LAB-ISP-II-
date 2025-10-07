11f15aab5335179d181ebd7791003b2fdc0e54a6
torchu1o <tudorsebastian.torcica@ulbsibiu.ro>
2025-10-07 10:58:32 +0300

    last modifiers

---
+++ src/Animal.java
@@ -0,0 +1,6 @@
+public class Animal {
+    void sound() {
+        System.out.println("s-a apelat");
+    }
+
+}
---
+++ src/Dog.java
@@ -0,0 +1,7 @@
+public class Dog extends Animal {
+
+    void sound() {
+        System.out.println("woof");
+    }
+
+}
